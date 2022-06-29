52b91e3d0395500ef387bc7511f0189e0706f07e
Joseph Sams <samsjb87@hotmail.com>
2022-06-29 18:57:34 -0400

    Setup empty store and provider. Add jsconfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import { store } from "@moon/app/store";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
