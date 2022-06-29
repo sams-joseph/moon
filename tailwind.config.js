1dbee324d1fefd7d41d9554be63e3afd4b7b502b
Joseph Sams <samsjb87@hotmail.com>
2022-06-29 18:33:47 -0400

    Init project with tailwind, redux, firebase, heroicons/ui, and react-firebase-hooks

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./common/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
